6ab963ec192e7c04a2141a60e9cdebe7786bae0a
王珠峰 <734765725@qq.com>
2022-06-24 01:46:23 +0800

    grpc getway

--- go.mod
+++ go.mod
@@ -3,14 +3,21 @@ module messageIdGenerator
 go 1.18
 
 require (
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.3
+	google.golang.org/genproto v0.0.0-20220617124728-180714bec0ad
 	google.golang.org/grpc v1.47.0
 	google.golang.org/protobuf v1.28.0
 )
 
 require (
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.0.0-20220607020251-c690dde0001d // indirect
-	golang.org/x/sys v0.0.0-20220610221304-9f5ed59c137d // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	golang.org/x/net v0.0.0-20220615171555-694bf12d69de // indirect
+	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20220608133413-ed9918b62aac // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main/main.go
+++ main/main.go
@@ -1,21 +1,112 @@
 package main
 
 import (
+	"context"
 	"log"
+	"messageIdGenerator/getMessageIGrpcGateway"
 	"net"
+	"net/http"
+	"sync"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 )
 
+type Server struct {
+	getMessageIGrpcGateway.UnimplementedGetMessageIdServiceServer
+}
+
+func (_this *Server) GetMessageId(ctx context.Context, request *getMessageIGrpcGateway.GetMessageRequest) (*getMessageIGrpcGateway.GetMessageResponse, error) {
+	log.Println("GetMessageId接收到参数\n\r", request)
+	return &getMessageIGrpcGateway.GetMessageResponse{
+		UserMessageId: "572387598302",
+	}, nil
+}
+
 func main() {
-	listener, err := net.Listen("tcp", ":8028")
-	if err != nil {
-		log.Println("tcp链接异常\n\r")
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(2)
+
+	go grpcServer(&wg)
+
+	go httpGetWay(&wg)
+
+	wg.Wait()
+
+}
+
+func httpGetWay(wg *sync.WaitGroup) {
+	defer func() {
+		wg.Done()
+	}()
+	//	http getway ******************************************************************
+	gwmux := runtime.NewServeMux()
+
+	//Http
+	gwServer := &http.Server{
+		Handler: gwmux,
+		Addr:    "127.0.0.1:8080",
 	}
-	log.Println("消息id生成器服务tcp端口:8028\n\r")
+
+	//转发服务
+	grpcClientConn, err := grpc.DialContext(
+		context.Background(),
+		"127.0.0.1:8028",
+		grpc.WithBlock(),
+		grpc.WithInsecure(),
+	)
+
+	if err != nil {
+		log.Println("getWay Http 转发异常\n\r", err)
+	} else {
+		log.Println("getWay Http 转发正常端口8080\n\r")
+	}
+	defer grpcClientConn.Close()
+
+	// 合并处理
+	err = getMessageIGrpcGateway.RegisterGetMessageIdServiceHandler(
+		context.Background(),
+		gwmux,
+		grpcClientConn,
+	)
+
+	if err != nil {
+		log.Println("getWay Http 处理异常\n\r", err)
+	} else {
+		log.Println("getWay Http 处理正常端口\n\r")
+	}
+
+	err = gwServer.ListenAndServe()
+	if err != nil {
+		log.Println("getWay Http 服务异常\n\r", err)
+	} else {
+		log.Println("getWay Http 服务正常端口8080\n\r")
+	}
+
+}
+
+func grpcServer(wg *sync.WaitGroup) {
+	defer func() {
+		wg.Done()
+	}()
+	//	grpc 服务 ******************************************************************
+
+	listener, err := net.Listen("tcp", "127.0.0.1:8028")
+	if err != nil {
+		log.Println("tcp链接异常\n\r", err)
+	} else {
+		log.Println("tcp链接正常端口8028\n\r")
+	}
+
 	grpcServer := grpc.NewServer()
+	//注册服务
+	getMessageIGrpcGateway.RegisterGetMessageIdServiceServer(grpcServer, &Server{})
 	err = grpcServer.Serve(listener)
 	if err != nil {
-		log.Println("grpcServer错误\n\r")
+		log.Println("grpcServer错误\n\r", err)
+	} else {
+		log.Println("grpcServer正常\n\r")
 	}
 }
